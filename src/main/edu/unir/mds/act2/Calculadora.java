8a25f9389af316913097630c0e3ca5afffb25e8c
Mauricio Mendoza <mauricio.mendozamm8@comunidadunir.net>
2025-04-16 15:04:57 -0400

    Agregando template de la calculadora

---
+++ src/main/edu/unir/mds/act2/Calculadora.java
@@ -0,0 +1,29 @@
+package edu.unir.mds.act2;
+
+public class Calculadora {
+
+    public double suma(double x, double y) {
+        return 0L;
+    }
+    
+    public double resta(double x, double y) {
+        return 0L;
+    }
+
+    public double multiplicacion(double x, double y) {
+        return 0L;
+    }
+    
+    public double division(double x, double y) {
+        return 0L;
+    }
+    
+    public double raiz(double x) {
+        return 0L;
+    }
+    
+    public double exponente(double x) {
+        return 0L;
+    }
+
+}
