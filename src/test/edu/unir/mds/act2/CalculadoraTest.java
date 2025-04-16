4091ccf2aaa2cd3d3be9553456316e87a07fbca1
Mauricio Mendoza <mauricio.mendozamm8@comunidadunir.net>
2025-04-16 14:59:02 -0400

    Agregando pruebas iniciales

---
+++ src/test/edu/unir/mds/act2/CalculadoraTest.java
@@ -0,0 +1,29 @@
+package edu.unir.mds.act2;
+
+public class CalculadoraTest {
+
+    public void pruebaSuma() {
+        
+    }
+
+    public void pruebaResta() {
+        
+    }
+    
+    public void pruebaMultiplicacion() {
+        
+    }
+
+    public void pruebaDivision() {
+        
+    }
+    
+    public void pruebaRaiz() {
+        
+    }
+
+    public void pruebaExponente() {
+        
+    }
+
+}
