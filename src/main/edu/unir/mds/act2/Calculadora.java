75d37a6eda04e4ee278943c2dfa0f9aa6ae321a8
Mauricio Mendoza <mauricio.mendozamm8@comunidadunir.net>
2025-04-16 15:15:28 -0400

    Eliminando casting de tipo de dato long

--- src/main/edu/unir/mds/act2/Calculadora.java
+++ src/main/edu/unir/mds/act2/Calculadora.java
@@ -3,27 +3,27 @@ package edu.unir.mds.act2;
 public class Calculadora {
 
     public double suma(double x, double y) {
-        return 0L;
+        return 0;
     }
     
     public double resta(double x, double y) {
-        return 0L;
+        return 0;
     }
 
     public double multiplicacion(double x, double y) {
-        return 0L;
+        return 0;
     }
     
     public double division(double x, double y) {
-        return 0L;
+        return 0;
     }
     
     public double raiz(double x) {
-        return 0L;
+        return 0;
     }
     
     public double exponente(double x) {
-        return 0L;
+        return 0;
     }
 
 }
