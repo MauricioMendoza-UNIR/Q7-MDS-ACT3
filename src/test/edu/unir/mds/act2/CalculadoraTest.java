c7275cbc56bfd88d909efab3a4716913410e2a5f
Mauricio Mendoza <mauricio.mendozamm8@comunidadunir.net>
2025-04-19 09:15:03 -0400

    Refactorizacion de pruebas
    Eliminando instancia de Calculadora, pues sus metodos ahora son estaticos.

--- src/test/edu/unir/mds/act2/CalculadoraTest.java
+++ src/test/edu/unir/mds/act2/CalculadoraTest.java
@@ -2,8 +2,6 @@ package edu.unir.mds.act2;
 
 public class CalculadoraTest {
     
-    private static final Calculadora CALC = new Calculadora();
-    
     public static void main(String[] argsv) {
         pruebaSuma();
         pruebaResta();
@@ -14,32 +12,32 @@ public class CalculadoraTest {
     }
 
     public static void pruebaSuma() {
-        assert CALC.suma(0.0, 0.0) == 0.0 : "Prueba de suma fallida :(";
+        assert Calculadora.suma(0.0, 0.0) == 0.0 : "Prueba de suma fallida :(";
         System.out.println("Pruebas de suma finalizadas con exito :)");
     }
 
     public static void pruebaResta() {
-        assert CALC.resta(0.0, 0.0) == 0.0;
+        assert Calculadora.resta(0.0, 0.0) == 0.0;
         System.out.println("Pruebas de resta finalizadas con exito :)");
     }
     
     public static void pruebaMultiplicacion() {
-        assert CALC.multiplicacion(0.0, 0.0) == 0.0;
+        assert Calculadora.multiplicacion(0.0, 0.0) == 0.0;
         System.out.println("Pruebas de multiplicacion finalizadas con exito :)");
     }
 
     public static void pruebaDivision() {
-        assert CALC.division(0.0, 1.0) == 0.0;
+        assert Calculadora.division(0.0, 1.0) == 0.0;
         System.out.println("Pruebas de division finalizadas con exito :)");
     }
     
     public static void pruebaRaiz() {
-        assert CALC.raiz(0.0) == 0.0;
+        assert Calculadora.raiz(0.0) == 0.0;
         System.out.println("Pruebas de raiz finalizadas con exito :)");
     }
 
     public static void pruebaExponente() {
-        assert CALC.exponente(0.0) == 1.0 : "Prueba de exponente fallida";
+        assert Calculadora.exponente(0.0) == 1.0 : "Prueba de exponente fallida";
         System.out.println("Pruebas de exponente finalizadas con exito :)");
     }
 
